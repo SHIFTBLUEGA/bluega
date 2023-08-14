68bfbacaab1f2e5bf0d30df20007dfe4acbe0184
BLUEGA <109780258+Lowkeyplay@users.noreply.github.com>
2023-08-14 21:44:19 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("login-form");
+    const message = document.getElementById("message");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        if (username === "114514" && password === "1919180") {
+            message.textContent = "登录成功！";
+            message.style.color = "green";
+			window.location.href="main.html"
+        } else {
+            message.textContent = "无效的用户名或密码。";
+            message.style.color = "red";
+        }
+    });
+});
